50d0afd1317d54040843e27146b38cbe1956b8c5
S.Panfilov <se-panfilov@ya.ru>
2015-06-30 12:43:48 +0800

    Fixes

--- src/messages.ts
+++ src/messages.ts
@@ -12,10 +12,12 @@ module apd.messages {
         };
 
         static throwModelValidationMessage (field:string) {
+            //TODO (S.Panfilov) possibly problems with this
             this.throwDeveloperError(this.messages.invalidDateModel + ': error on field \"' + field + '+\"');
         }
 
         static throwInvalidParamsMessage (){
+            //TODO (S.Panfilov) possibly problems with this
             this.throwDeveloperError(this.messages.invalidParams);
         }
     }
