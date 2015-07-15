639d6e59ac7af4c0a755b077faff81c00f16fbe8
S.Panfilov <se-panfilov@ya.ru>
2015-07-15 11:20:41 +0800

    fixes

--- src/modules/plunker.js
+++ src/modules/plunker.js
@@ -11,57 +11,61 @@ angular.module('app.plunker', [])
         STYLES: []
     })
 
-    .factory('plunkGenerator', function ($document, plunkerSettings) {
+    .directive('plunkerEdit', function ($document, plunkerSettings) {
 
-        return function (module, content) {
-
-            var form = angular.element('<form style="display: none;" method="post" action="http://plnkr.co/edit/?p=preview" target="_blank"></form>');
-            var addField = function (name, value) {
-                var input = angular.element('<input type="hidden" name="' + name + '">');
-                input.attr('value', value);
-                form.append(input);
-            };
-
-            var indexContent = function (content) {
-                return '<!doctype html>\n' +
-                    '<html ng-app="' + plunkerSettings.APP_NAME + '">\n' +
-                    '  <head>\n' +
-                    '    <script src="//ajax.googleapis.com/ajax/libs/angularjs/' + plunkerSettings.NG_VERSION + '/angular.js"></script>\n' +
-                    '    <script src="' + plunkerSettings.APP_URL + plunkerSettings.APP_VERSION + '.js"></script>\n' +
-                    '    <script src="example.js"></script>\n' +
-                    '  </head>\n' +
-                    '  <body>\n\n' +
-                    content + '\n' +
-                    '  </body>\n' +
-                    '</html>\n';
-            };
-
-            var scriptContent = function (content) {
-                return "angular.module('" + plunkerSettings.APP_NAME + "', ['" + plunkerSettings.APP_DEPS + "']);" + "\n" + content;
-            };
-
-            addField('description', plunkerSettings.APP_DESCRIPTION);
-            addField('files[index.html]', indexContent(content.markup, version));
-            addField('files[example.js]', scriptContent(content.javascript));
-
-            $document.find('body').append(form);
-            form[0].submit();
-            form.remove();
-        };
-    })
-
-    .controller('PlunkerCtrl', function ($scope, plunkGenerator) {
-        $scope.content = {};
-
-        $scope.edit = function (version, module) {
-            plunkGenerator(NG_VERSION, version, module, $scope.content);
-        };
-    })
-
-    .directive('plunkerContent', function () {
         return {
-            link: function (scope, element, attrs) {
-                scope.content[attrs.plunkerContent] = element.text().trim();
+            restrict: 'E',
+            scope: {
+                htmlContent: '=',
+                jsContent: '='
+            },
+            template: '<button class="btn btn-primary" ng-click="edit()">Edit in Plunker</button>',
+            link: function (scope) {
+                var form = angular.element('<form style="display: none;" method="post" action="http://plnkr.co/edit/?p=preview" target="_blank"></form>');
+
+                var addField = function (name, value) {
+                    var input = angular.element('<input type="hidden" name="' + name + '">');
+                    input.attr('value', value);
+                    form.append(input);
+                };
+
+                var indexContent = function () {
+                    return '<!doctype html>\n' +
+                        '<html ng-app="' + plunkerSettings.APP_NAME + '">\n' +
+                        '  <head>\n' +
+                        '    <script src="//ajax.googleapis.com/ajax/libs/angularjs/' + plunkerSettings.NG_VERSION + '/angular.js"></script>\n' +
+                        '    <script src="' + plunkerSettings.APP_URL + plunkerSettings.APP_VERSION + '.js"></script>\n' +
+                        '    <script src="example.js"></script>\n' +
+                        '  </head>\n' +
+                        '  <body>\n\n' +
+                        scope.htmlContent + '\n' +
+                        '  </body>\n' +
+                        '</html>\n';
+                };
+
+                var scriptContent = function () {
+                    return "angular.module('" + plunkerSettings.APP_NAME + "', ['" + plunkerSettings.APP_DEPS + "']);" +
+                        "\n" +
+                        scope.jsContent;
+                };
+
+                scope.edit = function () {
+                    addField('description', plunkerSettings.APP_DESCRIPTION);
+
+                    if (scope.htmlContent) {
+                        addField('files[index.html]', indexContent());
+                    }
+
+                    if (scope.jsContent) {
+                        addField('files[example.js]', scriptContent());
+                    }
+
+                    $document.find('body').append(form);
+                    form[0].submit();
+                    form.remove();
+                };
+
             }
-        }
-    });
+        };
+    })
+;
