8a402af96887ebbfbc81b7ea74fd6a2922b07493
S.Panfilov <se-panfilov@ya.ru>
2015-09-24 13:34:53 +0800

    fixes for tests

--- tests/00_Config.js
+++ tests/00_Config.js
@@ -1,14 +1,13 @@
 var expect = require('chai').expect;
-
 var config = require('../dist/x-date-core.js').Config;
 
+describe("Config tests", function() {
 
-
-describe("Module", function() {
-    console.log(config);
     it("is module an object", function() {
         expect(config).to.be.a('object');
+        expect(Object.getPrototypeOf(config)).to.be.a('object');
     });
+
 
     it("is module not empty", function() {
         expect(Object.keys(config)).to.have.length.greaterThan(0);
