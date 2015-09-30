420f79301d4b39893c8c42f3cd33586b279595bb
S.Panfilov <se-panfilov@ya.ru>
2015-09-30 15:00:20 +0800

    test

--- tests/03_LimitsModelTest.js
+++ tests/03_LimitsModelTest.js
@@ -4,6 +4,7 @@ var LimitsModel = require('../dist/x-date-core.test_only.js').LimitsModel;
 describe("LimitsModel", function () {
 
     it("is module a function", function () {
+        //TODO (S.Panfilov) didn't sure, may be it should be an object
         expect(LimitsModel).to.be.a('function');
     });
 
@@ -33,119 +34,165 @@ describe("LimitsModel", function () {
         expect(Object.getPrototypeOf(limitsModel.now)).to.be.a('object');
     });
 
-    describe("Common Case", function () {
-        it("Common Case check start", function () {
-            var year = 2015;
-            var month = 2;
-            var day = 3;
-            var start = new Date(year, month, day).getTime();
-            var end = new Date(year + 1, month + 1, day + 1).getTime();
+    describe("limitsModel object creation tests", function () {
+        var year;
+        var month;
+        var day;
+        var start;
+        var end;
+        var limitsModel;
+
+        beforeEach(function () {
+            year = 2015;
+            month = 2;
+            day = 3;
+            start = new Date(year, month, day).getTime();
+            end = new Date(year + 1, month + 1, day + 1).getTime();
+            limitsModel = new LimitsModel(start, end);
+        });
+
+        afterEach(function () {
+            year = null;
+            month = null;
+            day = null;
+            start = null;
+            end = null;
+            limitsModel = null;
+        });
+
+        describe("Common Case", function () {
+
+            beforeEach(function () {
+                limitsModel = new LimitsModel(start, end);
+            });
+
+            afterEach(function () {
+                limitsModel = null;
+            });
+
+            it("Common Case check start", function () {
+                expect(limitsModel.start.d).to.be.equal(day);
+                expect(limitsModel.start.m).to.be.equal(month);
+                expect(limitsModel.start.y).to.be.equal(year);
+                expect(limitsModel.start.dt).to.be.equal(start);
+            });
+
+            it("Common Case check end", function () {
+                expect(limitsModel.end.d).to.be.equal(day + 1);
+                expect(limitsModel.end.m).to.be.equal(month + 1);
+                expect(limitsModel.end.y).to.be.equal(year + 1);
+                expect(limitsModel.end.dt).to.be.equal(end);
+            });
+
+            it("Common Case check now", function () {
+                var expectedNow = new Date();
+
+                expect(limitsModel.now.d).to.be.equal(expectedNow.getDate());
+                expect(limitsModel.now.m).to.be.equal(expectedNow.getMonth());
+                expect(limitsModel.now.y).to.be.equal(expectedNow.getFullYear());
+                expect(limitsModel.now.dt).to.be.a('Number');
+            });
+        });
+
+        describe("No `new` keyword case", function () {
+
+            beforeEach(function () {
+                limitsModel = LimitsModel(start, end);
+            });
+
+            afterEach(function () {
+                limitsModel = null;
+            });
+
+            it("no `new` keyword check start", function () {
+                expect(limitsModel.start.d).to.be.equal(day);
+                expect(limitsModel.start.m).to.be.equal(month);
+                expect(limitsModel.start.y).to.be.equal(year);
+                expect(limitsModel.start.dt).to.be.equal(start);
+            });
+
+            it("no `new` keyword check end", function () {
+                expect(limitsModel.end.d).to.be.equal(day + 1);
+                expect(limitsModel.end.m).to.be.equal(month + 1);
+                expect(limitsModel.end.y).to.be.equal(year + 1);
+                expect(limitsModel.end.dt).to.be.equal(end);
+            });
+
+            it("no `new` keyword check now", function () {
+                var expectedNow = new Date();
+
+                expect(limitsModel.now.d).to.be.equal(expectedNow.getDate());
+                expect(limitsModel.now.m).to.be.equal(expectedNow.getMonth());
+                expect(limitsModel.now.y).to.be.equal(expectedNow.getFullYear());
+                expect(limitsModel.now.dt).to.be.a('Number');
+            });
+        });
+
+        describe("String number case", function () {
+
+            beforeEach(function () {
+                limitsModel = LimitsModel(start.toString(), end.toString());
+            });
+
+            afterEach(function () {
+                limitsModel = null;
+            });
+
+
+            it("no `new` keyword check start", function () {
+                expect(limitsModel.start.d).to.be.equal(day);
+                expect(limitsModel.start.m).to.be.equal(month);
+                expect(limitsModel.start.y).to.be.equal(year);
+                expect(limitsModel.start.dt).to.be.equal(start);
+            });
+
+            it("no `new` keyword check end", function () {
+                expect(limitsModel.end.d).to.be.equal(day + 1);
+                expect(limitsModel.end.m).to.be.equal(month + 1);
+                expect(limitsModel.end.y).to.be.equal(year + 1);
+                expect(limitsModel.end.dt).to.be.equal(end);
+            });
+
+            it("no `new` keyword check now", function () {
+                var expectedNow = new Date();
+
+                expect(limitsModel.now.d).to.be.equal(expectedNow.getDate());
+                expect(limitsModel.now.m).to.be.equal(expectedNow.getMonth());
+                expect(limitsModel.now.y).to.be.equal(expectedNow.getFullYear());
+                expect(limitsModel.now.dt).to.be.a('Number');
+            });
+        });
+
+        it("Common Case length check", function () {
+            var start = new Date(2015, 1, 1).getTime();
+            var end = new Date(2015, 1, 2).getTime();
             var limitsModel = new LimitsModel(start, end);
 
-            expect(limitsModel.start.d).to.be.equal(day);
-            expect(limitsModel.start.m).to.be.equal(month);
-            expect(limitsModel.start.y).to.be.equal(year);
-            expect(limitsModel.start.dt).to.be.equal(start);
+            var startLength = Object.keys(limitsModel.start).length;
+            var endLength = Object.keys(limitsModel.end).length;
+            var nowLength = Object.keys(limitsModel.now).length;
+
+            expect(limitsModel.start).to.be.a('object');
+            expect(startLength).to.be.greaterThan(0);
+            expect(endLength).to.be.greaterThan(0);
+            expect(nowLength).to.be.greaterThan(0);
         });
 
-        it("Common Case check end", function () {
-            var year = 2015;
-            var month = 2;
-            var day = 3;
-            var start = new Date(year, month, day).getTime();
-            var end = new Date(year + 1, month + 1, day + 1).getTime();
-            var limitsModel = new LimitsModel(start, end);
+        it("No args", function () {
+            var limitsModel = new LimitsModel();
+            var startLength = Object.keys(limitsModel.start).length;
+            var endLength = Object.keys(limitsModel.end).length;
+            var nowLength = Object.keys(limitsModel.now).length;
 
-            expect(limitsModel.end.d).to.be.equal(day + 1);
-            expect(limitsModel.end.m).to.be.equal(month + 1);
-            expect(limitsModel.end.y).to.be.equal(year + 1);
-            expect(limitsModel.end.dt).to.be.equal(end);
+            expect(limitsModel.start).to.be.a('object');
+            expect(startLength).equal(0);
+
+            expect(limitsModel.end).to.be.a('object');
+            expect(endLength).equal(0);
+
+            expect(limitsModel.now).to.be.a('object');
+            expect(nowLength).to.be.greaterThan(0);
         });
-
-        it("Common Case check now", function () {
-            var start = new Date(2015, 2, 3).getTime();
-            var end = new Date(2016, 4, 5).getTime();
-            var expectedNow = new Date();
-            var limitsModel = new LimitsModel(start, end);
-
-            expect(limitsModel.now.d).to.be.equal(expectedNow.getDate());
-            expect(limitsModel.now.m).to.be.equal(expectedNow.getMonth());
-            expect(limitsModel.now.y).to.be.equal(expectedNow.getFullYear());
-            expect(limitsModel.now.dt).to.be.a('Number');
-        });
-    });
-
-    describe("No `new` keyword case", function () {
-        it("no `new` keyword check start", function () {
-            var year = 2015;
-            var month = 2;
-            var day = 3;
-            var start = new Date(year, month, day).getTime();
-            var end = new Date(year + 1, month + 1, day + 1).getTime();
-            var limitsModel = LimitsModel(start, end);
-
-            expect(limitsModel.start.d).to.be.equal(day);
-            expect(limitsModel.start.m).to.be.equal(month);
-            expect(limitsModel.start.y).to.be.equal(year);
-            expect(limitsModel.start.dt).to.be.equal(start);
-        });
-
-        it("no `new` keyword check end", function () {
-            var year = 2015;
-            var month = 2;
-            var day = 3;
-            var start = new Date(year, month, day).getTime();
-            var end = new Date(year + 1, month + 1, day + 1).getTime();
-            var limitsModel = LimitsModel(start, end);
-
-            expect(limitsModel.end.d).to.be.equal(day + 1);
-            expect(limitsModel.end.m).to.be.equal(month + 1);
-            expect(limitsModel.end.y).to.be.equal(year + 1);
-            expect(limitsModel.end.dt).to.be.equal(end);
-        });
-
-        it("no `new` keyword check now", function () {
-            var start = new Date(2015, 2, 3).getTime();
-            var end = new Date(2016, 4, 5).getTime();
-            var expectedNow = new Date();
-            var limitsModel = LimitsModel(start, end);
-
-            expect(limitsModel.now.d).to.be.equal(expectedNow.getDate());
-            expect(limitsModel.now.m).to.be.equal(expectedNow.getMonth());
-            expect(limitsModel.now.y).to.be.equal(expectedNow.getFullYear());
-            expect(limitsModel.now.dt).to.be.a('Number');
-        });
-    });
-
-    it("Common Case length check", function () {
-        var start = new Date(2015, 1, 1).getTime();
-        var end = new Date(2015, 1, 2).getTime();
-        var limitsModel = new LimitsModel(start, end);
-
-        var startLength = Object.keys(limitsModel.start).length;
-        var endLength = Object.keys(limitsModel.end).length;
-        var nowLength = Object.keys(limitsModel.now).length;
-
-        expect(limitsModel.start).to.be.a('object');
-        expect(startLength).to.be.greaterThan(0);
-        expect(endLength).to.be.greaterThan(0);
-        expect(nowLength).to.be.greaterThan(0);
-    });
-
-    it("No args", function () {
-        var limitsModel = new LimitsModel();
-        var startLength = Object.keys(limitsModel.start).length;
-        var endLength = Object.keys(limitsModel.end).length;
-        var nowLength = Object.keys(limitsModel.now).length;
-
-        expect(limitsModel.start).to.be.a('object');
-        expect(startLength).equal(0);
-
-        expect(limitsModel.end).to.be.a('object');
-        expect(endLength).equal(0);
-
-        expect(limitsModel.now).to.be.a('object');
-        expect(nowLength).to.be.greaterThan(0);
     });
 
     describe("test privates", function () {
@@ -178,7 +225,6 @@ describe("LimitsModel", function () {
         });
 
         describe("setStart", function () {
-            //TODO (S.Panfilov) add upper for common case, case when string numbers passed
             it("Check start changed", function () {
                 var expectedDay = startDay + 5;
                 var expectedMonth = startMonth + 5;
@@ -292,7 +338,51 @@ describe("LimitsModel", function () {
                 expect(limitModel.now.y).to.be.equal(nowYear);
             });
 
+            describe("setEnd", function () {
 
+                it("Check end NOT changed", function () {
+                    expect(limitModel.end.dt).to.be.equal(end);
+                    expect(limitModel.end.d).to.be.equal(endDay);
+                    expect(limitModel.end.m).to.be.equal(endMonth);
+                    expect(limitModel.end.y).to.be.equal(endYear);
+
+                    limitModel._private._setStart(123);
+
+                    expect(limitModel.end.dt).to.be.equal(end);
+                    expect(limitModel.end.d).to.be.equal(endDay);
+                    expect(limitModel.end.m).to.be.equal(endMonth);
+                    expect(limitModel.end.y).to.be.equal(endYear);
+                });
+
+                it("Check start NOT changed", function () {
+                    expect(limitModel.start.dt).to.be.equal(start);
+                    expect(limitModel.start.d).to.be.equal(startDay);
+                    expect(limitModel.start.m).to.be.equal(startMonth);
+                    expect(limitModel.start.y).to.be.equal(startYear);
+
+                    limitModel._private._setEnd(123);
+
+                    expect(limitModel.start.dt).to.be.equal(start);
+                    expect(limitModel.start.d).to.be.equal(startDay);
+                    expect(limitModel.start.m).to.be.equal(startMonth);
+                    expect(limitModel.start.y).to.be.equal(startYear);
+                });
+
+                it("Check now changed", function () {
+                    var nowDateTime = limitModel.now.dt;
+
+                    expect(limitModel.now.dt).to.be.equal(nowDateTime);
+
+                    setTimeout(function () {
+                        limitModel._private._setNow();
+
+                        expect(limitModel.now.dt).to.be.above(nowDateTime);
+                    }, 100);
+
+                });
+
+
+            });
         });
     });
 });
