f722d2158556d830c2023fb72a97424df10ff197
Sergey Panfilov <sp@arex.io>
2016-03-09 14:05:55 +0800

    Fixes

--- src/DataClass.js
+++ src/DataClass.js
@@ -3,7 +3,7 @@
 /*END.DEV_ONLY*/
 
 /*START.TESTS_ONLY*/
-exports.DataClass = /*END.TESTS_ONLY*/ function (model, start, end) {
+exports.ListsState = /*END.TESTS_ONLY*/ function () {
   var exports = {
     list: {
       y: null,
@@ -21,19 +21,11 @@ exports.DataClass = /*END.TESTS_ONLY*/ function (model, start, end) {
     }
   };
 
-  model.dt = x.CommonUtils.isValidNumber(model.dt) ? model.dt : null;
-  start = x.CommonUtils.isValidNumber(start) ? start : null;
-  end = x.CommonUtils.isValidNumber(end) ? end : null;
-
-  //x.State.setLimits(start, end);
-  //x.State.setSelected(model.dt);
-
   exports.list.y = x.YearsUtils.getYearsList();
   exports.list.m = x.MonthUtils.getMonthList();
   exports.list.d = x.DaysUtils.getDaysList();
 
   //TODO (S.Panfilov) perhaps we should watch model and limits value here and update them
-  //TODO (S.Panfilov) And perhaps rename module to "Lists" and move model.dt away
 
   return exports;
 }/*START.TESTS_ONLY*/;
--- src/state.js
+++ src/state.js
@@ -4,27 +4,46 @@
 
 /*START.TESTS_ONLY*/
 exports.State = /*END.TESTS_ONLY*/ {
-  selected: {},
+  selected: {},//TODO (S.Panfilov) refactor selected
   setSelected: function (dt) {
-    this.selected = x.DateUtils.makeDmyModel(dt);
+    var result;
+
+    var isUpperStart = (dt > this.start.dt);
+    var isEqualStart = (dt === this.start.dt);
+    var isLowerEnd = (dt > this.end.dt);
+    var isEqualEnd = (dt === this.end.dt);
+
+    //start == 1; model == 1 or 2 or 3; end == 3;
+    if ((isUpperStart || isEqualStart) || (isLowerEnd || isEqualEnd)) {
+      result = new x.DateModel(dt);
+    } else if (!isUpperStart) { //start == 1; model == 0
+      result = new x.DateModel(this.start);
+    } else if (!isUpperStart) { //model == 4; end == 3;
+      result = new x.DateModel(this.end.dt);
+    } else {//paranoid case
+      result = new x.DateModel(+(new Date()));
+    }
+    
+    this.selected = result;
   },
   start: {},
   setStart: function (dt) {
-    this.start = x.DateUtils.makeDmyModel(dt);
+    this.start = new x.DateModel(dt);
+    this.start.isExist = true;
   },
   end: {},
   setEnd: function (dt) {
-    this.end = x.DateUtils.makeDmyModel(dt);
+    this.end = new x.DateModel(dt);
+    this.end.isExist = true;
   },
   setLimits: function (start, end) {
     this.setStart(start);
     this.setEnd(end);
   },
   resetNow: function () {
-    this.now = x.DateUtils.makeDmyModel(+(new Date()));
-    return this.now;
+    this.now = new x.DateModel(+(new Date()));
   },
-  now: this.resetNow()
+  now: null// new x.DateModel(+(new Date())) //TODO (S.Panfilov) FIX it - x is undefined
 }
   /*START.TESTS_ONLY*/;
 return exports;
